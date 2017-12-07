d09bf8e556315bf59ee0f1a4b380115f5394a9a7
vishnur12 <34345943+vishnur12@users.noreply.github.com>
2017-12-07 11:19:42 -0500

    added new class batch1

---
+++ Fisrtsample/src/com/icici/loans/homeloans/Batch1.java
@@ -0,0 +1,11 @@
+package com.icici.loans.homeloans;
+
+public class Batch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git class");
+
+	}
+
+}
