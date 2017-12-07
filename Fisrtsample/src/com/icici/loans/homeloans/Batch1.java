1d969431d7a194bfa9bf7e9669f12a18b63e3f80
vishnur12 <34345943+vishnur12@users.noreply.github.com>
2017-12-07 11:29:30 -0500

    New line added

--- Fisrtsample/src/com/icici/loans/homeloans/Batch1.java
+++ Fisrtsample/src/com/icici/loans/homeloans/Batch1.java
@@ -5,6 +5,7 @@ public class Batch1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git class");
+		System.out.println("Git server");
 
 	}
 
