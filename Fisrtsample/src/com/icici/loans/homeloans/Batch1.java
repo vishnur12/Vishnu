72e2e5186a9be299ca105b5dcb85ad1667537e65
vishnur12 <34345943+vishnur12@users.noreply.github.com>
2017-12-07 11:33:24 -0500

    Git batch

--- Fisrtsample/src/com/icici/loans/homeloans/Batch1.java
+++ Fisrtsample/src/com/icici/loans/homeloans/Batch1.java
@@ -1,11 +1,14 @@
 package com.icici.loans.homeloans;
 
+import org.apache.xerces.util.SynchronizedSymbolTable;
+
 public class Batch1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git class");
 		System.out.println("New git server");
+		System.out.println("new git line");
 
 	}
 
